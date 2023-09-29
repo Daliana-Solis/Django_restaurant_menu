0001f0b19c24136546e83a018d80d85d99f2c03a
Daliana-Solis <dsolisso@uci.edu>
2023-09-29 18:56:35 -0500

    Styling on item pages

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zrmcihz%p&3)+-=y8v+4#+4i^(l+e!l*0yv7o-7htzt9ocyd35'
+SECRET_KEY = "SECRET KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
